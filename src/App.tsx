c5d3f63a82b20a834ec336134d0df7fc253c7c2f
Max Shabalin <shabalinmaksimromanovich@gmail.com>
2022-11-25 00:02:31 +0300

    added routes and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import {Routes, Route, Link} from "react-router-dom";
+import './App.css'
+import ProjectsPage from "./pages/ProjectsPage";
+import CurrentProject from "./pages/CurrentProjectPage";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header>
+            <Link to={'/'}>
+                TODO
+            </Link>
+        </header>
+      <Routes>
+        <Route path={'/'} element={<ProjectsPage/>} />
+        <Route path={'/currentProject'} element={<CurrentProject/>} />
+      </Routes>
     </div>
   );
 }
